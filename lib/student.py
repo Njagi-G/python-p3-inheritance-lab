f8956c68eb9f5f35c603de4987ccb8a3b1755235
Njagi Githaiga <njagigithaiga@gmail.com>
2024-06-08 02:37:24 +0300

    Complete lab by augmenting functionality defined in the superclass with some additional behavior in the subclass.

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,24 @@
 from user import User
 
 class Student(User):
+
+    def __init__(self, first_name, last_name):
+        
+        super().__init__(first_name, last_name)
+
+        self.knowledge = []
     
-    def learn(self):
+    def learn(self, student_string):
         pass
+        return self.knowledge.append(student_string)
+
+
+mike = Student("Mike", "Sonko")
+
+print(mike.first_name, mike.last_name)
+
+mike.learn("JavaScript")
+
+print(mike.knowledge)
+
+        
